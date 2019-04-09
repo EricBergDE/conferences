c8dbef0381013ff2d216cc2b1cd5cfa41f979f6d
Eric Berg <info@ericberg.de>
2019-04-09 17:50:41 +0200

    Update ResourceGraph.sh

--- IntelligentCloudConference2019/DEMO/ResourceGraph.sh
+++ IntelligentCloudConference2019/DEMO/ResourceGraph.sh
@@ -1,19 +1,19 @@
 # summarize locations used
-az graph query -q 'summarize count() by location' --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q 'summarize count() by location' 
 
 # summarize number of RGs
-az graph query -q 'summarize Num_ResourceGroups = count(resourceGroup)' --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q 'summarize Num_ResourceGroups = count(resourceGroup)' 
 
 # summarize resource types per RG
-az graph query -q 'summarize Num_ResourceTypes = count() by resourceGroup' --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q 'summarize Num_ResourceTypes = count() by resourceGroup' 
 
 # summarize resource type most used
-az graph query -q "summarize count() by type| project resource=type , total=count_ | order by total desc" --output table --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q "summarize count() by type| project resource=type , total=count_ | order by total desc" --output table 
 az network public-ip create -l westus -g RG-App01 -n DEMO-PIP07
 az network public-ip list --output table
 
 # find public IPs
-az graph query -q "where type =~ 'Microsoft.Network/publicIPAddresses' | project name, location, resourceGroup" --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q "where type =~ 'Microsoft.Network/publicIPAddresses' | project name, location, resourceGroup" 
 
 # find unused public IPs
-az graph query -q "where type =~ 'Microsoft.Network/publicIPAddresses' and not(isnotempty(properties.ipConfiguration)) | project name, location, resourceGroup" --subscriptions 4b0e99c8-7a07-4496-833f-416ac5b8c9ef
+az graph query -q "where type =~ 'Microsoft.Network/publicIPAddresses' and not(isnotempty(properties.ipConfiguration)) | project name, location, resourceGroup" 
